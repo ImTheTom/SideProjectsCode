42ca03c0ff8b366c1575140edb44c0c9f09d29c9
Tom <Tom4310@gmail.com>
2020-04-05 16:10:17 +1000

    Added more options

--- chess/main.ai.py
+++ chess/main.ai.py
@@ -4,24 +4,42 @@ import chess
 import helpers
 board = chess.Board()
 
-while(not board.is_game_over()):
-	print(board.unicode(invert_color=True, borders=True))
-	move = input("Enter your move: ")
-	try:
-		board.parse_uci(move)
-		move = chess.Move.from_uci(move)
-		if (move != chess.Move.null()):
-			board.push(move)
-		else:
-			print("Move was equal to null could not perform")
-	except:
-		print("Could not perform that move")
-	if(board.turn == chess.BLACK):
-		bestMove = helpers.miniMaxRootAlpha(3, board, True)
-		print("Computer did move: " + bestMove.uci())
-		board.push(bestMove)
+RANDOM = "1"
+EVALUATED_MOVE = "2"
+MINIMAX = "3"
 
-if (board.turn):
-	print("Black won")
-else:
-	print("White won")
+OPTIONS = [RANDOM, EVALUATED_MOVE, MINIMAX]
+
+while(True):
+	gameType = input("Computer Intelligence\n\t1. Random moves\n\t2. Evaluated 1 move ahead\n\t3. Minimax algorithm 3 depth\n")
+	if gameType in OPTIONS:
+		while(not board.is_game_over()):
+			print(board.unicode(invert_color=True, borders=True))
+			move = input("Enter your move: ")
+			try:
+				board.parse_uci(move)
+				move = chess.Move.from_uci(move)
+				if (move != chess.Move.null()):
+					board.push(move)
+				else:
+					print("Move was equal to null could not perform")
+			except:
+				print("Could not perform that move")
+			if(board.turn == chess.BLACK):
+				if gameType == RANDOM:
+					bestMove = helpers.getRandomMove(board)
+				elif gameType == EVALUATED_MOVE:
+					bestMove = helpers.calculateBestMove(board)
+				elif gameType == MINIMAX:
+					bestMove = helpers.miniMaxRootAlpha(3, board, True)
+				board.push(bestMove)
+				print("Computer did move: " + bestMove.uci())
+
+		if (board.turn):
+			print("Black won")
+		else:
+			print("White won")
+		move = input("Enter to play again...")
+	else:
+		print("Sorry please select a valid option of 1, 2 or 3")
+
